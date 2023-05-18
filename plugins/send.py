fd66d183732ba57cdb96f2ffbb1fdd22ebe9fb22
AFzOfficial <abol.mortal50@gmail.com>
2023-05-18 14:16:41 +0330

    add: sender info in message

--- plugins/send.py
+++ plugins/send.py
@@ -1,9 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from models import get_unknown_with_unknown_id
-
-
+from models import get_unknown_with_unknown_id, get_unknown_with_real_id
 
 
 
@@ -12,13 +10,20 @@ from models import get_unknown_with_unknown_id
 
 @Client.on_message(filters.command('send') & filters.private)
 async def send(client: Client, message: Message):
-    unknown_id = await message.chat.ask('**Enter User ID:**')
-    unknown_message = await message.chat.ask('**Enter Message : **')
+    reciver_unknown_id = await message.chat.ask('**Enter User ID:**')
+    sender_message = await message.chat.ask('**Enter Message : **')
 
-    user = get_unknown_with_unknown_id(unknown_id.text)
+    sender  = get_unknown_with_real_id(str(message.chat.id))
+    reciver = get_unknown_with_unknown_id(reciver_unknown_id.text)
 
-    if user:
-        await Client.send_message(client, chat_id=user[0], text=unknown_message.text)
+    if reciver:
+
+        text = f'''**Message From:** `{sender[1] if sender else 'Unknown'}`
+
+{sender_message.text}
+'''
+
+        await Client.send_message(client, chat_id=reciver[0], text=text)
 
         await message.reply('**Message Sent.**')
     else:
